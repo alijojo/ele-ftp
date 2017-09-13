87b043c2137ab6068df579bbd17de234c974f991
songyazhao <59256790@qq.com>
2017-09-13 13:28:53 +0800

    Submit daily

---
+++ src/renderer/main.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import axios from 'axios'
+import Element from 'element-ui'
+
+Vue.use(Element)
+
+import App from './App'
+import router from './router'
+import store from './store'
+
+Vue.http = Vue.prototype.$http = axios
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  components: { App },
+  router,
+  store,
+  template: '<App/>'
+}).$mount('#app')
